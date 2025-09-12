3e80b39c06d274e880c94a4c8a49764bfc9fb367
sarthak <sarthak@tapplent.com>
2025-09-12 21:01:59 +0530

    default theme updated

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -6,6 +6,6 @@ return {
     require('catppuccin').setup {
       auto_integrations = true,
     }
-    vim.cmd.colorscheme 'catppuccin-latte'
+    vim.cmd.colorscheme 'catppuccin-macchiato'
   end,
 }
